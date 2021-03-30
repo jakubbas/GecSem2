374ca620c4fec38835fa736dd6ea06bdf2810b4b
Skoobaq <b014818k@student.staffs.ac.uk>
2021-03-30 01:37:36 +0100

    Finished Tutorial 2

--- MarioBaseProject/MarioBaseProject/Source.cpp
+++ MarioBaseProject/MarioBaseProject/Source.cpp
@@ -1,8 +1,65 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include "constants.h"
+#include <iostream>
+using namespace std; 
+SDL_Window* g_window = nullptr;
+
+bool InitSDL();
+void CloseSDL();
+
 
 int main(int argc, char* args[])
 {
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
+
 	return 0;
+}
+
+bool InitSDL()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+
+		return true;
+	}
+
+}
+
+void CloseSDL()
+{
+	//releases memory from window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+	//quits sdl subsystems
+	IMG_Quit();
+	SDL_Quit();
+
+
 }
